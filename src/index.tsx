32c9d8b43b8fa7300331cad2bf2da2c2f1356043
kaysonwu <772663139@qq.com>
2020-03-14 22:02:58 +0800

    update client entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Application from '@/components/application';
+// import 'core-js';
+// import 'regenerator-runtime';
 
 ReactDOM.render(
-  <div>Hello world!</div>,
+  <BrowserRouter>
+    <Application />
+  </BrowserRouter>,
   document.getElementById('root')
 );
