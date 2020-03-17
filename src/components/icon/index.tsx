6117e0e53e27c48c1dd126cee8644960e4006ca4
kaysonwu <772663139@qq.com>
2020-03-17 12:33:07 +0800

    add icon component

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const Icon = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+});
+
+export default Icon;
