063739540f97f69b74cd964f18d0adab53765bd9
kaysonwu <772663139@qq.com>
2020-03-23 18:58:44 +0800

    add zh-CN

---
+++ src/locales/zh-CN/index.ts
@@ -0,0 +1,11 @@
+import antd from 'antd/es/locale-provider/zh_CN';
+
+const zh_CN : Locale = {
+  antd,
+  "Home": "首页",
+  "User": "用户",
+  "User Level": "用户等级",
+  "User Manage": "用户管理",
+};
+
+export default zh_CN;
