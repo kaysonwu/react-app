311ea4bdcf955a9e8e4077149284b5190fb16998
kaysonwu <772663139@qq.com>
2020-03-26 19:11:56 +0800

    add preprocessor

--- src/components/locale-provider/loadable.tsx
+++ src/components/locale-provider/loadable.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import loadable from '@loadable/component';
+// #!if browser
 import ErrorBoundary from './boundary';
+// #!endif
+import { LocaleFile } from '@/utils/loadable';
 
 interface Props {
   paths: string[];
   children: (messages: ILocale) => React.ReactNode;
 }
 
-const LocaleFile = loadable.lib(
-  ({ path }: any) => import(`@/locales/${path}`), 
-  { cacheKey: props => `locales/${props.path}` }
-);
-
 export default ({ paths, children  }: Props) => {
   return paths.reduceRight((fn, path) => {
     return (messages: ILocale) => {
       return (
+        // #!if browser
         <ErrorBoundary fallback={() => fn(messages)}>
+        {/* #!endif */}
           <LocaleFile path={path}>
             { ({ default: _ }: any) => fn({ ...messages, ..._ }) }
           </LocaleFile>
+        {/* #!if browser */}
         </ErrorBoundary>
+        // #!endif
       );
     }
   }, children)({}) as JSX.Element;
