cf841c6a7c9473a67e7797bf58278b8025fe3aa6
kaysonwu <772663139@qq.com>
2020-04-02 09:26:32 +0800

    update app

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import Router from '../router';
+import React, { useEffect, useState } from 'react';
+import { Store } from 'redux';
+import { Provider } from 'react-redux';
+import LocaleProvider from '../locale-provider';
+import { getNameFromPath, getLocale } from '@/utils/locale'
 
-export default () => {
+export interface ApplicationProps {
+  locale: string;
+  store: Store;
+  location?: { pathname: string; }
+}
+
+export default ({ locale, store, location }: ApplicationProps) => {
+  
+  // #!if browser
+  let setLocale: any;
+  [locale, setLocale] = useState(locale);
+  // #!endif
+  const files = location ? [locale, `${locale}/${getNameFromPath(location.pathname)}`] : undefined;
+  
+  // #!if browser
+  const handleLanguageChange = (e: any) => {
+    setLocale(getLocale());
+  }
+
+
+  useEffect(() => {
+    return () => {
+
+    }
+  });
+  // #!endif
+
   return (
-    <Router />
+    <LocaleProvider locale={locale} files={files}>
+      <Provider store={store}>
+        <div>Hello ssr!</div>
+      </Provider>
+    </LocaleProvider>
   );
-};
+}
