1396b3d756ad82a60d4cf3a0a9488dcddce7ef8b
kaysonwu <772663139@qq.com>
2020-04-08 19:46:58 +0800

    change command name

--- scripts/artisan.js
+++ scripts/artisan.js
@@ -6,6 +6,6 @@ program
   .usage('command [options] [arguments]')
   .command('make:page', 'Create a new page file', { executableFile: './commands/make-page.js' })
   .command('make:model', 'Create a new model file', { executableFile: './commands/make-model.js' })
-  .command('make:server', 'Build server render ecosystem', { executableFile: './commands/make-server.js' })
+  .command('server:generate', 'Generate missing server render files, dependencies, and script commands', { executableFile: './commands/server-generate.js' })
   .parse(process.argv);
   
