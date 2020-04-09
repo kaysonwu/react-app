265fea57c5a288ff5abb16cb548283ed47b3a1bd
kaysonwu <772663139@qq.com>
2020-04-09 13:32:28 +0800

    add server:clear command

--- scripts/artisan.js
+++ scripts/artisan.js
@@ -7,5 +7,6 @@ program
   .command('make:page', 'Create a new page file', { executableFile: './commands/make-page.js' })
   .command('make:model', 'Create a new model file', { executableFile: './commands/make-model.js' })
   .command('server:generate', 'Generate missing server render files, dependencies, and script commands', { executableFile: './commands/server-generate.js' })
+  .command('server:clear', 'Clear server render files, dependencies, and script commands', { executableFile: './commands/server-clear.js' })
   .parse(process.argv);
   
