52b3339337e5670520371a7eee3770de23ca2bb7
kaysonwu <772663139@qq.com>
2020-04-10 10:33:42 +0800

    add loadable util

---
+++ src/utils/loadable.ts
@@ -0,0 +1,30 @@
+import { join } from 'path';
+import { existsSync } from 'fs';
+
+export function getNameFromPath(path: string) {
+  if (! path || path === '/') {
+    return 'home';
+  }
+  return path.replace(/^\/+/, '').replace('/', '-');
+}
+
+// #!if NODE_SERVER
+export function loadModel(names: string[], path: string) {
+  const models: IModel[] = [];
+  for (let name of names) {
+    try {
+      let filename = join(path, `${name}.js`);
+      let model = require(filename).default as IModel;
+      models.push(model);
+      if (model.dependencies) {
+        models.push(...loadModel(model.dependencies, path));
+      }
+    } catch {}
+  }
+  return models.filter((model, i) => ! models.splice(i + 1).some(m => m.id === model.id));
+}
+
+export function hasLocaleFile(name: string, path: string = join(__dirname, 'locales')) {
+  return existsSync(join(path, `${name}.js`));
+}
+// #!endif
