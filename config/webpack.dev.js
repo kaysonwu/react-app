adc36debffb6b5fa09e432ec4b1d0e795d8d10b0
kaysonwu <772663139@qq.com>
2020-04-12 13:59:58 +0800

    add mock

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,52 @@
+const { resolve, join } = require('path')
+const { parse } = require('url')
+const { existsSync } = require('fs')
+
+function resolveMockFile(pathname, root) {
+  const paths = pathname.split('/')
+  for (let i = paths.length; i > 0; i--) {
+    let filename = join(root, paths[i] + '.js')
+    if (existsSync(filename)) {
+      return filename    
+    }
+  }
+  return ''
+}
+
+function mockServe(root) {
+  return function(req, res, next) {
+    const { pathname } =  parse(req.url)
+    const filename = resolveMockFile(pathname, root)
+
+    if (!filename) {
+      return next()
+    }
+
+    const key = req.method + ' ' + pathname
+    const module = require(filename)
+    const type = typeof module[key]
+
+    if (type === 'function') {
+      return module[key](req, res, next)
+    } else if (type === 'object' || type == 'array') {
+      return res.json(module[key])
+    } else if (type === 'string') {
+      return res.send(module[key])
+    }
+
+    return next()
+  }
+}
+
+const mock = mockServe(resolve(__dirname, '..', 'mocks'))
+
 module.exports = {
   devtool: false,
   devServer: {
     open: true,
-    historyApiFallback: true    
+    historyApiFallback: true,
+    after: (app) => {
+      app.all('*', mock)
+    }
   }
-};
+}
---
+++ mocks/v1.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'GET /v1/getUserInfo': { id: 1, name: 'kayson' }
+}
