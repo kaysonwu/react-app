c478296a7c39f7c9a195b0ad8321a2fc1bc01753
kaysonwu <772663139@qq.com>
2020-04-13 13:29:15 +0800

    update locale type

--- src/typings/locale.d.ts
+++ src/typings/locale.d.ts
@@ -1,61 +1,11 @@
-// @reference rc-field-form
-type ValidateMessage = string | (() => string);
+interface ILocale {
 
-// @reference rc-field-form
-interface ValidateMessages {
-    default?: ValidateMessage;
-    required?: ValidateMessage;
-    enum?: ValidateMessage;
-    whitespace?: ValidateMessage;
-    date?: {
-        format?: ValidateMessage;
-        parse?: ValidateMessage;
-        invalid?: ValidateMessage;
-    };
-    types?: {
-        string?: ValidateMessage;
-        method?: ValidateMessage;
-        array?: ValidateMessage;
-        object?: ValidateMessage;
-        number?: ValidateMessage;
-        date?: ValidateMessage;
-        boolean?: ValidateMessage;
-        integer?: ValidateMessage;
-        float?: ValidateMessage;
-        regexp?: ValidateMessage;
-        email?: ValidateMessage;
-        url?: ValidateMessage;
-        hex?: ValidateMessage;
-    };
-    string?: {
-        len?: ValidateMessage;
-        min?: ValidateMessage;
-        max?: ValidateMessage;
-        range?: ValidateMessage;
-    };
-    number?: {
-        len?: ValidateMessage;
-        min?: ValidateMessage;
-        max?: ValidateMessage;
-        range?: ValidateMessage;
-    };
-    array?: {
-        len?: ValidateMessage;
-        min?: ValidateMessage;
-        max?: ValidateMessage;
-        range?: ValidateMessage;
-    };
-    pattern?: {
-        mismatch?: ValidateMessage;
-    };
-}
-
-type PageLocale = Record<string, any>;
-
-interface ILocale extends PageLocale {
+  [key: string]: any;
 
   /**
    * Antd component language pack.
+   * 
+   * @example import zh_CN from antd/lib/locale-provider/zh_CN
    */
-  antd?: any;
+  antd?: import('antd/lib/locale-provider').Locale;
 }
