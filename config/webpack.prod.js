60cd3da2420f9a8cfecd5722ae7e476408591c32
kaysonwu <772663139@qq.com>
2020-04-16 17:30:44 +0800

    update webpack prod config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,17 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['**/*', '!images', '!images/**/*', '!.gitignore', '!update-browser.html']
-    })
-  ]
+      cleanOnceBeforeBuildPatterns: [
+        '**/*', 
+        '!images', 
+        '!images/**/*', 
+        '!.gitignore', 
+        '!update-browser.html',
+        '!*-stats.json',
+      ],
+    }),
+  ],
 };
