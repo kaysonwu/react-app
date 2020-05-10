6947f2fc3413539d796c1b531299ef0cb55339ec
kaysonwu <772663139@qq.com>
2020-05-10 15:36:50 +0800

    update types

--- src/typings/locale.d.ts
+++ src/typings/locale.d.ts
@@ -8,4 +8,9 @@ interface ILocale {
    * @example import zh_CN from antd/lib/locale-provider/zh_CN
    */
   antd?: import('antd/lib/locale-provider').Locale;
+
+  /**
+   * Antd form component validate messages.
+   */
+  validateMessages?: import('rc-field-form/lib/interface').ValidateMessages;
 }
