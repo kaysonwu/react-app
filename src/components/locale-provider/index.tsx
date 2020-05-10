765a28fcd223483cb19e7e201c0b3f4b679311f1
kaysonwu <772663139@qq.com>
2020-05-10 16:40:30 +0800

    fix

--- src/components/locale-provider/index.tsx
+++ src/components/locale-provider/index.tsx
@@ -4,7 +4,7 @@ import { IntlProvider } from 'react-intl';
 import { ConfigProviderProps } from 'antd/lib/config-provider';
 import Loadable from '../loadable/locale';
 
-interface LocaleProviderProps extends Omit<ConfigProviderProps, 'locale'> {
+interface LocaleProviderProps extends Omit<ConfigProviderProps, 'locale' | 'form'> {
   locale: string;
   // Language files. path relative to @/locales
   files?: string[]; 
@@ -13,9 +13,22 @@ interface LocaleProviderProps extends Omit<ConfigProviderProps, 'locale'> {
 const LocaleProvider = ({ locale, files, children, ...props }: LocaleProviderProps) => {
   return (
     <Loadable paths={files || [locale]}>
-      {({ antd, ...messages }: any) => {
+      {({ antd, validateMessages, ...messages }: ILocale) => {
         const intl = <IntlProvider locale={locale} messages={messages}>{children}</IntlProvider>
-        return antd ? <ConfigProvider {...props} locale={antd}>{intl}</ConfigProvider> : intl;
+
+        if (!antd || !validateMessages) {
+          return intl;
+        }
+
+        return (
+          <ConfigProvider 
+            {...props} 
+            locale={antd} 
+            form={{ validateMessages }}
+          >
+            {intl}
+          </ConfigProvider>
+        );
       }}
     </Loadable>
   );
