7979ddc103eb6c01d296b45e213ac292ba19858d
kaysonwu <772663139@qq.com>
2020-05-10 15:40:32 +0800

    update home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import Page from '@/components/page';
 
-const Home = (props: any) => {
-
-  useEffect(() => {
-    console.log('Home 挂载了!');
-    return () => {
-      console.log('Home 卸载了!');
-    };
-  })
-
+const Home = () => {
   return (
     <Page>
-      <div>This is home page!</div>
-      <Link to="/user">Go User</Link>
+      <FormattedMessage id="Start your imagination here!" />
     </Page>
   );
 }
