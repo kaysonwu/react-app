c03b493db9a06453f2cf110b9f3a686cd464e353
kaysonwu <772663139@qq.com>
2020-05-16 22:17:04 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,55 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
+    jest: true,
   },
   extends: [
     'airbnb',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
+    'airbnb/hooks',
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: [
-    'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
   rules: {
-  }
+    'react/jsx-filename-extension': ['error', { 
+      extensions: ['.tsx'],
+    }],
+    'import/no-extraneous-dependencies': ['error', { 
+      devDependencies: true,
+    }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        'arrow-parens': ['error', 'as-needed'],
+        'react/prop-types': 'off',
+        'react/function-component-definition': ['error', { 
+          namedComponents: 'arrow-function', 
+          unnamedComponents: 'arrow-function' 
+        }],
+        '@typescript-eslint/prefer-includes': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/prefer-optional-chain': 'error',
+        '@typescript-eslint/no-extra-parens': 'error',
+      },
+    },
+  ],
 };
