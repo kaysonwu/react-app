2c2c51df15dc3eeb66f6e1e96e27d13f0ad75d62
kaysonwu <772663139@qq.com>
2020-06-02 23:45:23 +0800

    update request util

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
 import { stringify } from 'qs';
 
+export type ParamsType = Record<string, string | string[]>;
+
+/**
+ * Use params to rewrite the methods, the response result follow the `no-unnecessary-generics` rule.
+ *
+ * @see https://github.com/Microsoft/dtslint/blob/master/docs/no-unnecessary-generics.md
+ */
+interface AxiosInstanceWithParams extends AxiosInstance {
+  delete(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
+  get(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
+  head(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
+  options(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
+}
+
 const { transformRequest } = axios.defaults;
-const instance = axios.create({
+const instance: AxiosInstanceWithParams = axios.create({
   paramsSerializer(params) {
     return stringify(params);
   },
@@ -24,40 +39,35 @@ const instance = axios.create({
   },
 });
 
-instance.interceptors.response.use(response => {
-  return response.data;
-}, error => {
-  // TODO Global error handle.  
-});
-
-/**
- * Use params to rewrite the methods, the response result follow the `no-unnecessary-generics` rule.
- * 
- * @see https://github.com/Microsoft/dtslint/blob/master/docs/no-unnecessary-generics.md
- */
-interface AxiosInstanceWithParams extends AxiosInstance {
-  delete(url: string, params?: any, config?: AxiosRequestConfig): Promise<any>;
-  get(url: string, params?: any, config?: AxiosRequestConfig): Promise<any>;
-  head(url: string, params?: any, config?: AxiosRequestConfig): Promise<any>;
-  options(url: string, params?: any, config?: AxiosRequestConfig): Promise<any>;
-}
-
-['delete', 'get', 'head', 'options'].reduce((instance, method: any) => {
-  // @ts-ignore
-  instance[method] = function(url: string, params?: any, config?: AxiosRequestConfig) {
-    return instance.request({ ...config, method, url, params });
+instance.interceptors.response.use(({ data: response }) => {
+  if (Object.prototype.hasOwnProperty.call(response, 'current_page')) {
+    return {
+      data: response.data,
+      pagination: {
+        showQuickJumper: true,
+        showSizeChanger: true,
+        current: response.current_page,
+        pageSize: response.per_page,
+        total: response.total,
+        pageSizeOptions: ['15', '30', '50', '100'],
+      },
+    };
   }
 
-  return instance;
-}, instance);
+  return response;
+}, () => {
+  // TODO Global error handle.
+});
 
-export const { 
-  request, 
-  delete: del, 
-  get, 
-  head, 
-  options, 
-  post, 
-  put, 
-  patch,
-} = instance as AxiosInstanceWithParams;
+type Method = 'delete' | 'get' | 'head' | 'options';
+const mehtods: Method[] = ['get', 'delete', 'head', 'options'];
+
+mehtods.forEach(method => {
+  instance[method] = (
+    url: string,
+    params?: ParamsType,
+    config?: AxiosRequestConfig,
+  ) => instance.request({ ...config, method, url, params });
+});
+
+export const { getUri, request, get, delete: del, head, options, post, patch, put } = instance;
