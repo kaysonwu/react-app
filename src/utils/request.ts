39eea42cc4429270c223e373669627f32e6ea62d
kaysonwu <772663139@qq.com>
2020-06-17 18:37:22 +0800

    fix request bug

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,29 +1,50 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
+import axios, { AxiosRequestConfig, AxiosInterceptorManager, AxiosResponse } from 'axios';
 import { stringify } from 'qs';
+import { message } from 'antd';
+import { FormInstance } from 'antd/lib/form';
 
 export type ParamsType = Record<string, string | string[]>;
 
-/**
- * Use params to rewrite the methods, the response result follow the `no-unnecessary-generics` rule.
- *
- * @see https://github.com/Microsoft/dtslint/blob/master/docs/no-unnecessary-generics.md
- */
-interface AxiosInstanceWithParams extends AxiosInstance {
-  delete(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
-  get(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
-  head(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
-  options(url: string, params?: ParamsType, config?: AxiosRequestConfig): Promise<any>;
+interface RequestConfig extends AxiosRequestConfig {
+  withoutInterceptor?: true | number | number[];
+  form?: FormInstance;
+}
+
+export interface Response<T = any> extends AxiosResponse<T> {
+  config: RequestConfig;
+}
+
+type MethodHandler<T = any, R = Response<T>> = (
+  url: string,
+  params?: ParamsType,
+  config?: RequestConfig,
+) => Promise<R> | Promise<T>;
+
+interface AxiosInstance {
+  interceptors: {
+    request: AxiosInterceptorManager<RequestConfig>;
+    response: AxiosInterceptorManager<Response>;
+  };
+  getUri(config?: RequestConfig): string;
+  request<T = any, R = Response<T>> (config: RequestConfig): Promise<R> | Promise<T>;
+  get: MethodHandler;
+  delete: MethodHandler;
+  head: MethodHandler;
+  options: MethodHandler;
+  post: MethodHandler;
+  put: MethodHandler;
+  patch: MethodHandler;
 }
 
 const { transformRequest } = axios.defaults;
-const instance: AxiosInstanceWithParams = axios.create({
+const instance = axios.create({
   paramsSerializer(params) {
     return stringify(params);
   },
   transformRequest(data, headers) {
     // Plain object are resolved to querystring first.
-    if (toString.call(data) === '[object Object]') {
+    if (Object.prototype.toString.call(data) === '[object Object]') {
       return stringify(data);
     }
 
@@ -37,9 +58,31 @@ const instance: AxiosInstanceWithParams = axios.create({
 
     return data;
   },
-});
+}) as AxiosInstance;
 
-instance.interceptors.response.use(({ data: response }) => {
+function convertValidationErrorToFieldData(response: any) {
+  const { errors } = response;
+
+  // For Laravel:
+  // {"message":"The given data was invalid.","errors":{"field": ["error message"]}}
+  if (errors !== undefined) {
+    return Object.keys(errors)
+      .map(name => ({ name, errors: errors[name], value: undefined }));
+  }
+
+  // For Customize
+  // {"field": "first error message"}
+  return Object.keys(response)
+    .map(name => ({ name, erros: [response[name]], value: undefined }));
+}
+
+instance.interceptors.response.use(res => {
+  const { config: { withoutInterceptor, form }, data: response } = res;
+
+  if (withoutInterceptor) {
+    return res;
+  }
+
   if (Object.prototype.hasOwnProperty.call(response, 'current_page')) {
     return {
       data: response.data,
@@ -54,19 +97,49 @@ instance.interceptors.response.use(({ data: response }) => {
     };
   }
 
+  if (form) {
+    form.resetFields();
+
+    if (typeof response === 'string') {
+      message.success(response || window.intl.formatMessage({ id: 'Successful operation' }));
+      return true;
+    }
+  }
+
   return response;
-}, () => {
-  // TODO Global error handle.
+}, error => {
+  const { config: { withoutInterceptor, form }, status, data } = error.response as Response;
+  const statuses = typeof withoutInterceptor === 'number' ? [withoutInterceptor] : withoutInterceptor;
+
+  if (!statuses
+    || statuses === true
+    || !statuses.includes(status)
+  ) {
+    switch (status) {
+      case 422:
+        if (typeof data === 'string') {
+          message.error(data);
+        } else {
+          form?.setFields(convertValidationErrorToFieldData(data));
+        }
+        return;
+      default:
+        // TODO 根据错误码渲染错误
+        return;
+    }
+  }
+
+  throw error;
 });
 
 type Method = 'delete' | 'get' | 'head' | 'options';
-const mehtods: Method[] = ['get', 'delete', 'head', 'options'];
+const methods: Method[] = ['get', 'delete', 'head', 'options'];
 
-mehtods.forEach(method => {
+methods.forEach(method => {
   instance[method] = (
     url: string,
     params?: ParamsType,
-    config?: AxiosRequestConfig,
+    config?: RequestConfig,
   ) => instance.request({ ...config, method, url, params });
 });
 
