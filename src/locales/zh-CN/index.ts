9b09068559a63f736d69734945659200975bf8fc
kaysonwu <772663139@qq.com>
2020-07-14 19:49:45 +0800

    update locale

--- src/locales/zh-CN/index.ts
+++ src/locales/zh-CN/index.ts
@@ -63,6 +63,15 @@ const zhCN: Locale = {
   'Edit': '编辑',
   'Delete': '删除',
   'Action': '操作',
+  'Download': '下载',
+  'Home': '首页',
+  'User': '用户',
+  'User Level': '用户等级',
+  'User Manage': '用户管理',
+
+  // Toolbar
+  'Tools': '工具箱',
+  'Please check the columns to be exported': '请勾选需要导出的列',
   'Setting Column': '自定义列',
   'Column Display': '列展示',
   'Pin Left': '向左固定',
@@ -72,14 +81,17 @@ const zhCN: Locale = {
   'Unfixed': '不固定',
   'Right Fixed': '固定在右侧',
 
-  'Download': '下载',
-  'Home': '首页',
-  'User': '用户',
-  'User Level': '用户等级',
-  'User Manage': '用户管理',
-
-  // Toolbar
-  'Tools': '工具箱',
+  // Exception
+  'Back Home': '返回首页',
+  'Go Back': '回到之前',
+  'Try Again': '重新尝试',
+  'Forbidden': '访问受限',
+  'Sorry, you don\'t have access to this page.': '抱歉！你没有该页面的访问权限！',
+  'Not Found': '页面不存在',
+  'Sorry, the page you visited does not exist.': '抱歉！你访问的页面不存在！',
+  'Internal Server Error': '服务器错误',
+  'Sorry, the server is reporting an error.': '抱歉！服务器发生错误，请重新尝试！',
+  'Successful operation': '操作成功',
 };
 
 export default zhCN;
