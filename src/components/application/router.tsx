b1c112830c7e4ff78f9db871f3686ea4697712dc
kaysonwu <772663139@qq.com>
2020-07-14 23:23:10 +0800

    update route

--- src/components/application/router.tsx
+++ src/components/application/router.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { FC, useEffect } from 'react';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+import { Spin } from 'antd';
 import Page from '../loadable/page';
+import { Basic } from '../layout';
 
-function Router() {
+const Router: FC = () => {
+  let loading;
+
+  // #if WEB
+  const intl = useIntl();
+  const history = useHistory();
+
+  // eslint-disable-next-line prefer-const
+  loading = <Spin />;
+
+  useEffect(() => {
+    window.intl = intl;
+    window.route = history;
+  }, [intl, history]);
+  // #endif
+
   return (
-    <Switch>
-      <Route path="/user">
-        <Page path="user" />
-      </Route>
-      <Route path="/exception/404">
-        <Page path="exception/404" />
-      </Route>
-      <Route exact path="/">
-        <Page path="home" />
-      </Route>
-      <Redirect from="*" to="/exception/404" />
-    </Switch>
+    <Basic>
+      <Switch>
+        <Route path="/user">
+          <Page path="user" fallback={loading} />
+        </Route>
+        <Route path="/exception/404">
+          <Page path="exception/404" fallback={loading} />
+        </Route>
+        <Route exact path="/">
+          <Page path="home" fallback={loading} />
+        </Route>
+        <Redirect from="*" to="/exception/404" />
+      </Switch>
+    </Basic>
   );
-}
+};
 
 export default Router;
