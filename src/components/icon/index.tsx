a00c4cd53e7b6630c09a132a83f72b88d1f66e8c
kaysonwu <772663139@qq.com>
2020-07-14 22:34:58 +0800

    update icon

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -3,11 +3,11 @@ import { createFromIconfontCN } from '@ant-design/icons';
 import { IconFontProps } from '@ant-design/icons/lib/components/IconFont';
 
 const Icon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+  scriptUrl: '//at.alicdn.com/t/font_1852826_xt01kjyciwn.js',
 });
 
 // eslint-disable-next-line react/function-component-definition
-export function normalizeIcon(icon: ReactNode, props?: IconFontProps) {
+export function normalizeIcon(icon: ReactNode, props?: Omit<IconFontProps, 'type'>) {
   return typeof icon === 'string' ? <Icon {...props} type={icon} /> : icon;
 }
 
