417e1f26761cb8e7c87402ab81f65bc17e42c4bc
kaysonwu <772663139@qq.com>
2021-04-22 14:27:37 +0800

    add pluralize

--- src/utils/loadable.ts
+++ src/utils/loadable.ts
@@ -1,3 +1,5 @@
+import { singular } from 'pluralize';
+
 // #if WEB
 export function onLoadError() {
   // do nothing
@@ -9,8 +11,9 @@ export function getNameFromPath(path: string) {
     return 'home';
   }
 
-  // TODO 复数转单数，目前是仅去除结尾的 s
-  return path.replace(/^\/|s$/g, '')
-    .replace(/\/\d+\//, '/') // a/:id/b => a/b
-    .replace(/\//g, '-'); // a/b => a-b
+  return singular(
+    path.replace(/\/\d+\//, '/') // a/:id/b => a/b
+      .replace(/^\/|\/$/g, '')
+      .replace(/\//g, '-'), // a/b => a-b
+  );
 }
