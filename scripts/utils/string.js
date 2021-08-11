eb58e7dc96cfead526e6a72df5d2ffaf9cbe59b4
kaysonwu <772663139@qq.com>
2021-08-11 20:40:03 +0800

    rm scripts

--- scripts/utils/string.js
+++
@@ -1,9 +0,0 @@
-function ucfirst(string) {
-  return string[0].toUpperCase() + string.slice(1);
-}
-
-function ucwords(string) {
-  return string.replace(/\b(\w)|\s(\w)/g, m => m.toUpperCase());
-}
-
-module.exports = { ucfirst, ucwords };
